d0751cb949bbfec265d1123193212764b8ff9dbf
Sabyasachi Dash <sdash0113@gmail.com>
2023-11-04 23:09:31 +0530

    config done

--- insert data json/script.js
+++ insert data json/script.js
@@ -3,7 +3,9 @@ const ConLoginBtn = document.getElementById('ConLoginBtn')
 const loginContainer = document.getElementById('container-login')
 const Inputdatafield = document.getElementById('data-field')
 const btndiv = document.getElementById('btn-div')
+
 btndiv.innerHTML = ""
+
 NavLoginBtn.addEventListener('click',HaveContent)
 
 function HaveContent(e){
@@ -15,7 +17,22 @@ function HaveContent(e){
 ConLoginBtn.addEventListener('click',addJSONdata)
 function addJSONdata(){
     loginContainer.innerHTML= `<textarea name="" id="data-field" cols="60" rows="10"></textarea><br>
-    <button id="Submit-btn"  class="btn btn-outline-info">Submit</button>
+    <div id="btn-div"><button id="Submit-btn"  class="btn btn-outline-info">Submit</button></div>
     <h5>No Data</h5>`
+    console.log(loginContainer);
+}
+
+function storeData() {
+    
+    const jsonData = Inputdatafield.value;
+
+    if (jsonData) {
+        
+        const parsedData = JSON.parse(jsonData);
+        localStorage.setItem('jsonData', JSON.stringify(parsedData));
+        alert('Data stored in local storage.');
+    } else {
+        alert('No data to store.');
+    }
 }
 
