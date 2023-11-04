d81743c7a34572e32d80650aac0d9e021d8f3923
Sabyasachi Dash <sdash0113@gmail.com>
2023-11-05 00:00:54 +0530

    config done

--- insert data json/script.js
+++ insert data json/script.js
@@ -1,33 +1,32 @@
 const NavLoginBtn = document.getElementById('NavLoginBtn')
-const ConLoginBtn = document.getElementById('ConLoginBtn')
 const loginContainer = document.getElementById('container-login')
-const Inputdatafield = document.getElementById('data-field')
 const btndiv = document.getElementById('btn-div')
+let Inputdatafield;
 
 btndiv.innerHTML = ""
-
 NavLoginBtn.addEventListener('click',HaveContent)
 
 function HaveContent(e){
     NavLoginBtn.innerText = "Logout"
     e.preventDefault()
-    btndiv.innerHTML = `<button id="ConLoginBtn"  class="btn btn-outline-warning">Login</button>`
+    btndiv.innerHTML = `<button id="ConLoginBtn"  class="btn btn-outline-warning">Login</button>`   
 }
 
-ConLoginBtn.addEventListener('click',addJSONdata)
+btndiv.addEventListener('click',addJSONdata)
 function addJSONdata(){
-    loginContainer.innerHTML= `<textarea name="" id="data-field" cols="60" rows="10"></textarea><br>
-    <div id="btn-div"><button id="Submit-btn"  class="btn btn-outline-info">Submit</button></div>
+    loginContainer.innerHTML = `<textarea name="" id="data-field" cols="60" rows="10"></textarea><br>
+    <button id="Submit-btn"  class="btn btn-outline-info">Submit</button>
     <h5>No Data</h5>`
-    console.log(loginContainer);
+    const SubmitBtn = document.getElementById('Submit-btn');
+    SubmitBtn.addEventListener('click', storeData);
+
+     Inputdatafield = document.getElementById('data-field')
 }
+
 
 function storeData() {
-    
     const jsonData = Inputdatafield.value;
-
     if (jsonData) {
-        
         const parsedData = JSON.parse(jsonData);
         localStorage.setItem('jsonData', JSON.stringify(parsedData));
         alert('Data stored in local storage.');
@@ -35,4 +34,3 @@ function storeData() {
         alert('No data to store.');
     }
 }
-
